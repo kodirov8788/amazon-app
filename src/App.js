4795a0f8aa3c57af01d72037713a16219fa9c206
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-07-15 15:02:29 +0500

    created navbar header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from "./components/Header/Header"
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>salom</h1>
+      <Header />
+      <Navbar />
     </div>
   );
 }
