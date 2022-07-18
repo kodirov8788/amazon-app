8f1a7d2799d050301ea870c0adb2861778e29f30
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-07-18 14:24:46 +0500

    added reactrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-
+import Returns from "./pages/return/Returns";
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Returns />
     </div>
   );
 }
