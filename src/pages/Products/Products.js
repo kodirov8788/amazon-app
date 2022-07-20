3f808c294214edf95c3ee86fdc1b856968c5c314
Maqsudbek6665 <ahmadovmaqsudbek062@gmail.com>
2022-07-20 14:07:45 +0500

    maqsudbe pushed

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import "./Products.css"
-import { ProductData } from "../../static/static__data"
-import { Link } from "react-router-dom"
-
+import React from "react";
+import "./Products.css";
+import { ProductData } from "../../static/static__data";
+import { Link } from "react-router-dom";
 
 const Products = () => {
-    console.log(ProductData);
-    return (
-        <div className='products'>
-            {
-                ProductData.map(product => (
-                    <div className="products__item" key={product.id}>
-                        <Link state={product.id} to={`/product/${product.id}`}><h1>{product.ProductName}</h1></Link>
-                        <img style={{ width: "200px" }} src={product.ProductImage.type} alt="" />
-                    </div>
-                ))
-            }
+  console.log(ProductData);
+  return (
+    <div className="products">
+      {ProductData.map((product) => (
+        <div className="products__item" key={product.id}>
+          <img
+            style={{ width: "290px", height: "256" }}
+            src={product.ProductImage.type}
+            alt=""
+          />
+          <Link state={product.id} to={`/product/${product.id}`}>
+            <h4>{product.ProductName}</h4>
+          </Link>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Products
+export default Products;
